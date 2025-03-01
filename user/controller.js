d92d8a0763744d1117a9473c66640596ad62e60a
Hrishabh Jain <hrishabh038@gmail.com>
2025-03-01 13:52:58 +0530

    Production Commit: User routes done

--- user/controller.js
+++ user/controller.js
@@ -72,15 +72,134 @@ export const login = async (req, res) => {
     }
 
     // Generate JWT token
-    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
-      expiresIn: "9h",
-    });
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "9h",
+      }
+    );
 
     return res.status(200).json({
       success: true,
       message: "Logged in successfully",
       token,
       data: user.username,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { username, email_address, full_name, password, bio } = req.body;
+    const user_username = req.params.username;
+    const updateData = {};
+
+    // Check and update each field if provided
+    if (username) updateData.username = username;
+    if (email_address) updateData.email_address = email_address;
+    if (full_name) updateData.full_name = full_name;
+    if (bio) updateData.bio = bio;
+
+    // If the password is provided, hash it before updating
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      updateData.password = await bcrypt.hash(password, salt);
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { username: user_username },
+      updateData
+    );
+
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: updatedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+// Delete user route by username
+export const deleteUser = async (req, res) => {
+  try {
+    const user_username = req.params.username;
+
+    const deletedUser = await User.findOneAndDelete({
+      username: user_username,
+    });
+
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: deletedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+// Get all users route
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: users,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+// Get user by username route
+export const getUserByUsername = async (req, res) => {
+  try {
+    const user_username = req.params.username;
+
+    const user = await User.findOne({ username: user_username });
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User retrieved successfully",
+      data: user,
     });
   } catch (error) {
     return res.status(500).json({
--- user/route.js
+++ user/route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { register, login } from './controller.js';
+import { register, login, updateUser, deleteUser, getAllUsers, getUserByUsername } from './controller.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.put('/:username', updateUser);
+router.delete('/:username', deleteUser);
+router.get('/:username', getUserByUsername);
+router.get('/', getAllUsers);
 
 export default router;
